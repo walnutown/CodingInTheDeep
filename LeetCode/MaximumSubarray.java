909479ab4db4bdcd5849750787d08001c2d92664
walnutown <walnutown@gmail.com>
2013-08-27 13:30:10 -0700

    update

--- LeetCode/MaximumSubarray.java
+++ LeetCode/MaximumSubarray.java
@@ -1,3 +1,4 @@
+// partial works
 public class Solution {
     
     public int maxSubArray(int[] A) {
@@ -10,17 +11,10 @@ public class Solution {
         if (len == 1){
             return A[0];
         }
-        int subStart = 0;
-        int subEnd = 1;
         int disSum = 0;
         int[] mem = new int[len];
         mem[0] = A[0];
         for (int i = 1; i < len; i++){
-            if (A[i] <= 0){
-                disSum += A[i]; 
-                mem[i] = mem[i-1];
-            }
-            else{
                 int left = mem[i-1];
                 int right = A[i];
                 int both = left + disSum + right;
@@ -29,18 +23,90 @@ public class Solution {
                     disSum += A[i];
                 }
                 else if (mem[i] == right){
-                    subStart = i;
-                    subEnd = i+1;
                     disSum = 0;
                 }
                 else{
-                    subEnd = i +1;
                     disSum = 0;
                 }
+        }
+        
+        return mem[len-1];  
+    }  
+}
+
+// O(n) solution
+public class Solution {
+    public int maxSubArray(int[] A) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        int len = A.length;
+        if (A == null || len == 0 ){
+            return 0;
+        }
+        
+        int max = Integer.MIN_VALUE;
+        int sum = 0;
+        
+        for(int i = 0; i < len; i++){
+            sum += A[i];
+            max = Math.max(max, sum);
+            // if sum < 0, neg plus A[i], equals that A[i] is reduced
+            // so, we just need to throw away the previous sum
+            if (sum < 0){
+                sum = 0;
             }
         }
         
-        return mem[len];  
-    }  
+        return max;
+    }
 }
+
+
+// divide and conquer
+public class Solution {
+    int len;
+    public int maxSubArray(int[] A) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        
+        len = A.length;
+        if (A == null || len == 0){
+            return 0;
+        }
+        
+        int max = Integer.MIN_VALUE;
+        return findMax(A, 0, len -1, max);
+        
+    }
+    
+    public int findMax(int[] A, int start, int end, int max){
+        if (start > end){
+            return Integer.MIN_VALUE;
+        }
+        int mid = start + (end - start) /2;
+        int lMax = findMax(A, start, mid-1, max);
+        int rMax = findMax(A, mid+1, end, max);
+        max = Math.max(Math.max(lMax, rMax), max);
+        int sum = 0;
+        int lmaxSum = 0;
+        for (int i = mid-1; i >= start; i--){
+            sum += A[i];
+            lmaxSum = Math.max(lmaxSum, sum);
+            
+        }
+        
+        sum = 0;
+        int rmaxSum = 0;
+        for (int i = mid + 1; i <= end; i++){
+            sum += A[i];
+            rmaxSum = Math.max(rmaxSum, sum);
+        }
+        
+        max = Math.max(max, rmaxSum + lmaxSum + A[mid]);
+        return max;
+    }
+    
+}
+
+
 
