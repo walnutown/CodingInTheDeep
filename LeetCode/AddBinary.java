147e79c985e70e8141963affc797d98a058e822f
walnutown <walnutown@gmail.com>
2013-08-28 20:09:49 -0700

    update

--- LeetCode/AddBinary.java
+++ LeetCode/AddBinary.java
@@ -60,3 +60,37 @@ public class Solution {
         
     }
 }
+
+
+public class Solution {
+    public ArrayList<String> anagrams(String[] strs) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        ArrayList<String> res = new ArrayList<String>();
+        if (strs == null || strs.length == 0){
+            return res;
+        }
+        Map<String, Integer> map = new HashMap<String, Integer>();
+        String[] newStrs = new String[strs.length];
+        
+        for (int i = 0; i < strs.length; i++){
+            char[] charStr = strs[i].toCharArray();
+            Arrays.sort(charStr);
+            newStrs[i] = new String(charStr);
+            if (!map.containsKey(newStrs[i])){
+                map.put(newStrs[i], i);
+            }
+            else {
+                if (int prev = map.get(newStrs[i]) >= 0){
+                    res.add(strs[prev]);
+                    map.put(newStrs[i], -1);
+                }
+                res.add(strs[i]);
+            }
+        }
+        
+        
+        return res;
+    }
+}
+
