b9f1ce6ee5c142d00d78e7745d81624edf1e7ab9
walnutown <walnutown@gmail.com>
2013-08-30 01:21:09 -0700

    Dp again

--- LeetCode/DistinctSubsequence.java
+++ LeetCode/DistinctSubsequence.java
@@ -36,3 +36,40 @@ public class Solution {
         
     }
 }
+
+
+
+public class Solution {
+    public int numDistinct(String S, String T) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        int slen = S.length();
+        int tlen = T.length();
+        if (slen < tlen){
+            return 0;
+        }
+        
+        int[][] mem = new int[slen+1][tlen+1];
+        mem[0][0] = 1;
+        
+        for(int i = 1; i < tlen; i++){
+            mem[0][i] = 0;
+        }
+        
+        for(int i = 1; i < slen; i++){
+            mem[i][0] = 1;
+        }
+        
+        for (int i = 1; i<= slen; i++){
+            for (int j = 1; j <= tlen; j++){
+                if (S.charAt(i-1) == T.charAt(j-1)){
+                    mem[i][j] = mem[i-1][j] + mem[i-1][j-1];
+                }
+                else{
+                    mem[i][j] = mem[i-1][j];
+                }
+            }
+        }
+        return mem[slen][tlen];
+    }
+}
