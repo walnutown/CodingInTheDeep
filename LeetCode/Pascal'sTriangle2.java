a6787a0a8a9c3c3949e9f73b7575501b2976db2f
walnutown <walnutown@gmail.com>
2013-08-29 20:20:43 -0700

    pascalTriangle2

--- LeetCode/Pascal'sTriangle2.java
+++ LeetCode/Pascal'sTriangle2.java
@@ -15,12 +15,13 @@ public class Solution {
         
         ArrayList<Integer> prev = new ArrayList<Integer>(res);
         ArrayList<Integer> curr = new ArrayList<Integer>();
-        curr.add(1);
+        for(int i = 0; i <= rowIndex; i++){
+            curr.add(1);
+        }
         for(int i = 2; i <= rowIndex; i++){
-            for(int j = 1; j < prev.size(); j++){
+            for(int j = i-1; j >= 1; j--){
                 curr.set(j, prev.get(j-1) + prev.get(j));
             }
-            curr.add(1);
             prev = curr;
         }
         
