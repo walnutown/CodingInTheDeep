f12020c1de840c3d57026e12d60e02b751995995
walnutown <walnutown@gmail.com>
2013-09-01 10:20:17 -0700

    rotateImage pass judge

--- LeetCode/RotateImage.java
+++ LeetCode/RotateImage.java
@@ -8,8 +8,8 @@ public class Solution {
         }
         
         for (int layer = 0; layer < len/2; layer++){
-            int fisrt = layer;
-            int last = n-1- layer;
+            int first = layer;
+            int last = len-1- layer;
             for (int i = first; i< last; i++){
                 int offset = i-first;
                 // save top
@@ -17,8 +17,11 @@ public class Solution {
                 // left -> top
                 matrix[first][i] = matrix[last-offset][first];
                 // bottom -> left
-                matrix[last-offset][first] 
-                
+                matrix[last-offset][first] = matrix[last][last-offset];
+                // right -> bottom
+                matrix[last][last-offset] = matrix[i][last];
+                // top -> right
+                matrix[i][last] = top;
             }
         }
     }
