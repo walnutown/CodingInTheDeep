8fe17c1a9a9a4c7ea58ad631641eef4b535ee0a9
walnutown <walnutown@gmail.com>
2013-12-24 00:25:35 -0800

    word ladder

--- LeetCode/WordLadder.java
+++ LeetCode/WordLadder.java
@@ -57,3 +57,119 @@ public class Solution {
         return res;
     }
 }
+
+
+
+// TLE, Dec 23, DFS
+public class Solution {
+    private int min;
+    public int ladderLength(String start, String end, HashSet<String> dict) {
+        // DFS with visited set
+        Set<String> visited = new HashSet<String>();
+        visited.add(start);
+        min = 0;
+        finder(start, end, dict, visited, 1);
+        return min;
+    }
+    
+    public void finder(String start, String end, HashSet<String> dict, Set<String> visited, int cost){
+        if (start.equals(end)){
+            min = Math.min(min, cost);
+            return;
+        }
+        Set<String> adjacents = getAdjacents(start, dict);
+        for (String adj : adjacents){
+            if (visited.contains(adj))  continue;
+            visited.add(adj);
+            finder(adj, end, dict, visited, cost+1);
+            visited.remove(adj);
+        }
+    }
+    
+    public Set<String> getAdjacents(String start, HashSet<String> dict){
+        Set<String> adjacents = new HashSet<String>();
+        StringBuilder sb = new StringBuilder(start);
+        for (int i = 0; i < start.length(); i++){
+            char curr = start.charAt(i);
+            for (char j = 'a'; j <= 'z'; j++){
+                if (curr == j)  continue;
+                sb.setCharAt(i, j);
+                String adj = sb.toString();
+                if (dict.contains(adj))
+                    adjacents.add(adj);
+            }
+        }
+        return adjacents;
+    }   
+}
+
+// BFS, TLE
+public class Solution {
+    public int ladderLength(String start, String end, HashSet<String> dict) {
+        // BFS with visited set
+        Set<String> visited = new HashSet<String>();
+        int cost = 1;
+        int curr_num = 1, next_num = 0;
+        Queue<String> qu = new LinkedList<String>();
+        qu.add(start);
+        while (!qu.isEmpty()){
+            String curr = qu.poll();
+            visited.add(curr);
+            for (int i = 0; i < start.length(); i++) {
+                for (char j = 'a'; j <= 'z'; j++) {
+                    StringBuilder sb = new StringBuilder(curr);
+                    sb.setCharAt(i, j);
+                    String adj = sb.toString();
+                    if (adj.equals(end))    return cost + 1;
+                    if (dict.contains(adj) && !visited.contains(adj)){
+                        qu.add(adj);
+                        next_num++;
+                    }
+                }
+            }
+            curr_num--;
+            if (curr_num == 0){
+                curr_num = next_num;
+                next_num = 0;
+                cost++;
+            }
+        }
+        return 0;
+    }
+}
+
+// BFS, Accepted, Dec 23
+public class Solution {
+    public int ladderLength(String start, String end, HashSet<String> dict) {
+        // BFS with visited set
+        Set<String> visited = new HashSet<String>();
+        int cost = 1;
+        int curr_num = 1, next_num = 0;
+        Queue<String> qu = new LinkedList<String>();
+        qu.add(start);
+        visited.add(start);
+        while (!qu.isEmpty()){
+            String curr = qu.poll();
+            for (int i = 0; i < start.length(); i++) {
+                for (char j = 'a'; j <= 'z'; j++) {
+                    StringBuilder sb = new StringBuilder(curr);
+                    sb.setCharAt(i, j);
+                    String adj = sb.toString();
+                    if (adj.equals(end))    return cost + 1;
+                    if (dict.contains(adj) && !visited.contains(adj)){
+                        qu.add(adj);        
+                        visited.add(adj);   // just move visited.add() here, and pass the case
+                        next_num++;         // can avoid more duplicates, because size of visited increases faster than the above
+                    }
+                }
+            }
+            curr_num--;
+            if (curr_num == 0){
+                curr_num = next_num;
+                next_num = 0;
+                cost++;
+            }
+        }
+        return 0;
+    }
+}
