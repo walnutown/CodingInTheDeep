127548efe09e2d2179a5bc5d225b9ed6dcb52cdb
walnutown <walnutown@gmail.com>
2014-04-05 00:05:40 -0700

    longest common subsequence

--- Company/src/facebook/LongestCommonSubString.java
+++ Company/src/facebook/LongestCommonSubString.java
@@ -4,6 +4,15 @@ import org.junit.Test;
 
 public class LongestCommonSubString {
    /*
+    * A professor wants to see if two students have cheated when writing a paper. Design a function
+    * : hasCheated(String s1,String s2, int N) that evaluates to true if two strings have a common
+    * substring of length N. Additional question after implementation. Assume you don't have the
+    * possibility of using String.contains() and String.substring(). How would you implement this?
+    */
+   // This is actually the longest common substring problem, if the length of LCS is larger than N,
+   // the we can say that there exist two strings have a common substring of N. See Below.
+
+   /*
     * Given two strings a and b, find the length of the longest common substring. For example,
     * if the given strings are GeeksforGeeks and GeeksQuiz, the output should be 5 as longest
     * common substring is Geeks
@@ -29,7 +38,7 @@ public class LongestCommonSubString {
       for (int i = 1; i <= M; i++) {
          for (int j = 1; j <= N; j++) {
             if (a.charAt(i - 1) == b.charAt(j - 1)) {
-               dp[i][j] = dp[i - 1][j - 1] + a.substring(i-1, i);
+               dp[i][j] = dp[i - 1][j - 1] + a.substring(i - 1, i);
                if (lcs.length() < dp[i][j].length())
                   lcs = dp[i][j];
             } else
---
+++ Company/src/facebook/LongestCommonSubsequence.java
@@ -0,0 +1,45 @@
+package facebook;
+
+import org.junit.Test;
+
+public class LongestCommonSubsequence {
+   /*
+    * Given two sequences, find the length of longest subsequence present in both of them. A
+    * subsequence is a sequence that appears in the same relative order, but not necessarily
+    * contiguous. For example, abc, abg, bdf, aeg, acefg, .. etc are subsequences of
+    * abcdefg. 
+    */
+   // A string of length n has 2^n different possible subsequences.
+   // Use case: 'diff'
+   
+   // Dynamic Programming
+   // Maintain a 2d matrix to store the longest common subsequence
+   // dp[i][j] holds the longest common subsequence of string a.substring(0,i) and b.substring(0,j)
+   // time: O(m*n); space: O(m*n*Min(m,n))
+   public String findLongestCommonSubsequence(String a, String b){
+   		if (a==null || b==null)
+   			return "";
+   		int M = a.length(), N = b.length();
+   		String[][] dp = new String[M+1][N+1];
+   		for (int i=0; i<=M; i++)
+   		   dp[i][0] = "";
+   		for (int j=0; j<=N; j++)
+   		   dp[0][j] = "";
+   		for (int i=1; i<=M; i++){
+   			for (int j=1; j<=N; j++){
+   				if (a.charAt(i-1)==b.charAt(j-1)){
+   					dp[i][j] = dp[i-1][j-1] + a.substring(i-1,i);
+   				}else
+   					dp[i][j] = dp[i-1][j].length()> dp[i][j-1].length()?dp[i-1][j] : dp[i][j-1];
+   			}
+   		}
+   		return dp[M][N];
+   }
+
+   @Test
+   public void test(){
+   		String a = "abcdefg", b = "adegm";
+   		System.out.println(findLongestCommonSubsequence(a,b));
+   }
+   
+}
