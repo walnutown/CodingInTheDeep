035ebe930f2082d2a7ae1652ec51c07a688e5957
walnutown <walnutown@gmail.com>
2014-04-21 15:37:14 -0700

    add youngTableau

--- Company/src/google/SodaWaterMachine.java
+++ Company/src/google/SodaWaterMachine.java
@@ -1,10 +1,50 @@
 package google;
 
+import lib.Interval;
+
+import org.junit.Test;
+
 public class SodaWaterMachine {
    /*
     * A soda water machine,press button A can generate 300-310ml, button B can generate 400-420ml
     * and button C can generate 500-515ml, then given a number range [min, max], tell if all the
     * numbers of water in the range can be generated.
     */
-   // 
+   // This question is similar to famous coinChange, we can call it rangeCoinChange
+
+   // Dynamic Programming
+   // Maintain a boolean array to check whether a volume can be generated
+   // time: O(max * rangeNum * averageRange); space: O(max)
+   public boolean canBeGenerated(Interval[] ranges, Interval target) {
+      int min = target.start, max = target.end;
+      boolean[] dp = new boolean[max + 1];
+      dp[0] = true;
+      for (int i = 0; i <= max; i++) {
+         for (int j = 0; j < ranges.length; j++) {
+            Interval range = ranges[j];
+            for (int k = range.start; k <= range.end; k++) {
+               if (i >= k && dp[i - k])
+                  dp[i] = true;
+            }
+         }
+      }
+      for (int i = min; i <= max; i++) {
+         if (!dp[i])
+            return false;
+      }
+      return true;
+   }
+
+   @Test
+   public void test() {
+      Interval[] ranges = new Interval[] { 
+            new Interval(300, 310), 
+            new Interval(400, 402),
+            new Interval(500, 515) 
+      };
+      Interval target = new Interval(500,516);
+      Interval target2 = new Interval(500,515);
+      System.out.println(canBeGenerated(ranges, target));
+      System.out.println(canBeGenerated(ranges, target2));
+   }
 }
--- Company/src/google/YoungTableau.java
+++ Company/src/google/YoungTableau.java
@@ -1,9 +1,62 @@
 package google;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+import org.junit.Test;
+
 public class YoungTableau {
    /*
-    * 
+    * Given a N*N Matrix.
+    * All rows are sorted, and all columns are sorted.
+    * Find the Kth Largest element of the matrix.
     */
-   // http://lyle.smu.edu/~saad/courses/cse3358/ps5/problemset5sol.pdf
    // http://www.careercup.com/question?id=6335704
+   
+   // This is actually a variant of merge sort.
+   // Each row in the matrix can be regarded as a array of size N
+   // Then the question becomes to merge sort M arrays of size N
+   // time: O(klgk); sapce: O(k)
+   public ArrayList<Integer> getKthLargestInYoungTableau(int[][] matrix, int k){
+      int M = matrix.length, N = matrix[0].length;
+      ArrayList<Integer> res = new ArrayList<Integer>();
+      PriorityQueue<Cell> minHeap = new PriorityQueue<Cell>(k, new Comparator<Cell>(){
+         public int compare(Cell c1, Cell c2){
+            return c1.val - c2.val;
+         }
+      });
+      for (int i=0; i<M; i++)
+         minHeap.add(new Cell(i, 0, matrix[i][0]));
+      while (--k>=0 && !minHeap.isEmpty()){
+         Cell curr = minHeap.poll();
+         if (curr.y+1<N)
+            minHeap.add(new Cell(curr.x, curr.y+1, matrix[curr.x][curr.y+1]));
+         res.add(curr.val);
+      }
+      return res;
+   }
+   
+   class Cell{
+      int x;
+      int y;
+      int val;
+      public Cell(int x, int y, int val){
+         this.x = x;
+         this.y = y;
+         this.val = val;
+      }
+   }
+   
+   @Test
+   public void test(){
+      int[][] matrix = new int[][]{
+            {1,3,5,7},
+            {2,4,6,8},
+            {3,5,7,9},
+            {4,6,8,10}
+      };
+      System.out.println(getKthLargestInYoungTableau(matrix, 3));
+      System.out.println(getKthLargestInYoungTableau(matrix, 16));
+   }
 }
